62e42344987baf56bddf05bdd30454b9b1a10368
Himank Chaudhary <himank@tigrisdata.com>
2022-02-01 22:22:34 -0800

    Add Insert/Read GRPC test

---
+++ client/user_api_test.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"io"
+	"testing"
+
+	"github.com/rs/zerolog/log"
+	"github.com/stretchr/testify/require"
+	api "github.com/tigrisdata/tigrisdb/api/server/v1"
+	"google.golang.org/protobuf/types/known/structpb"
+)
+
+func TestAPIGRPC(t *testing.T) {
+	ctx := context.TODO()
+
+	c, err := newGRPCClient(ctx, "localhost", 8081)
+	require.NoError(t, err)
+
+	_, _ = c.DropCollection(ctx, &api.DropCollectionRequest{Db: "db1", Collection: "t1"})
+
+	values, err := structpb.NewValue([]interface{}{"pkey_int"})
+	require.NoError(t, err)
+	_, err = c.CreateCollection(ctx, &api.CreateCollectionRequest{Db: "db1", Collection: "t1", CreateBody: &api.CreateCollectionRequestBody{
+		Schema: &structpb.Struct{
+			Fields: map[string]*structpb.Value{
+				"primary_key": values,
+			},
+		}},
+	})
+	require.NoError(t, err)
+
+	inputDocuments := []*api.UserDocument{
+		{
+			Doc: &structpb.Struct{
+				Fields: map[string]*structpb.Value{
+					"pkey_int":  structpb.NewNumberValue(1),
+					"int_value": structpb.NewNumberValue(2),
+					"str_value": structpb.NewStringValue("foo"),
+				},
+			},
+		},
+	}
+	_, err = c.Insert(ctx, &api.InsertRequest{
+		Db:         "db1",
+		Collection: "t1",
+		InsertBody: &api.InsertRequestBody{
+			Documents: inputDocuments,
+		},
+	})
+	require.NoError(t, err)
+
+	rc, err := c.Read(ctx, &api.ReadRequest{Db: "db1", Collection: "t1", ReadBody: &api.ReadRequestBody{
+		Keys: []*api.UserDocument{
+			{
+				Doc: &structpb.Struct{
+					Fields: map[string]*structpb.Value{
+						"pkey_int": structpb.NewNumberValue(1),
+					},
+				},
+			},
+		},
+	}})
+	require.NoError(t, err)
+
+	totalReceivedDocuments := 0
+	for {
+		d, err := rc.Recv()
+		if err == io.EOF {
+			break
+		}
+		require.NoError(t, err)
+		log.Debug().Str("value", d.Doc.String()).Msg("Read")
+		require.EqualValues(t, inputDocuments[0].Doc.AsMap(), d.Doc.AsMap())
+		totalReceivedDocuments++
+	}
+	require.Equal(t, totalReceivedDocuments, len(inputDocuments))
+
+	_, err = c.DropCollection(ctx, &api.DropCollectionRequest{Db: "db1", Collection: "t1"})
+	require.NoError(t, err)
+
+	err = c.Close()
+	require.NoError(t, err)
+}
