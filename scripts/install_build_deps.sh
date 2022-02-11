a58ad581b3eabe8a1a149f6c081946234815b7db
Yevgeniy Firsov <firsov@tigrisdata.com>
2022-02-10 22:29:04 -0800

    Add lint Github action

--- scripts/install_build_deps.sh
+++ scripts/install_build_deps.sh
@@ -13,7 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
+set -ex
+
+export GO111MODULE=on
 
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.27.1
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2.0
@@ -33,7 +35,7 @@ else
 	FDB_SHA512=06ae7fc9e404118f0a707094eafe709e7a918483e8694abe7e601c1e46d1fdeed9fd9c538d8a8252f50f284a573dd735ba81ab54d9aba65b00690b8be90f0b43
 	wget https://github.com/apple/foundationdb/releases/download/$FDB_VERSION/foundationdb-clients_$FDB_VERSION-1_amd64.deb
 	echo "$FDB_SHA512 foundationdb-clients_$FDB_VERSION-1_amd64.deb" | sha512sum -c
-	dpkg -i foundationdb-clients_$FDB_VERSION-1_amd64.deb # provides /lib/libfdb_c.so shared libarry in the docker for CGO
+	sudo dpkg -i foundationdb-clients_$FDB_VERSION-1_amd64.deb # provides /lib/libfdb_c.so shared libarry in the docker for CGO
+	sudo apt-get install -y protobuf-compiler
 fi
-
 
