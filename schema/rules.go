c7e9b22f46f74d0fdf2d7ffe560264dbab3c8ec5
Jigar Joshi <jigarjm@gmail.com>
2022-08-16 19:14:53 -0700

    chore: Added missing license headers for test files

--- schema/rules.go
+++ schema/rules.go
@@ -1,3 +1,17 @@
+// Copyright 2022 Tigris Data, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package schema
 
 import (
@@ -56,13 +70,13 @@ func (v *FieldSchemaValidator) Validate(existing *DefaultCollection, current *Fa
 
 // ApplySchemaRules is to validate incoming collection request against the existing present collection. It performs
 // following validations,
-//  - Primary Key Changed, or order of fields part of the primary key is changed
-//  - Collection name change
-//  - Type of existing field is changed
-//  - A validation on field property is also applied like for instance if existing field has some property but it is
-//    removed in the new schema
-//  - Removing a field
-//  - Any index exist on the collection will also have same checks like type, etc
+//   - Primary Key Changed, or order of fields part of the primary key is changed
+//   - Collection name change
+//   - Type of existing field is changed
+//   - A validation on field property is also applied like for instance if existing field has some property but it is
+//     removed in the new schema
+//   - Removing a field
+//   - Any index exist on the collection will also have same checks like type, etc
 func ApplySchemaRules(existing *DefaultCollection, current *Factory) error {
 	if existing.Name != current.Name {
 		return ErrCollectionNameMismatch
