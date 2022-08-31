80acc57fcda7b425cab5f06a01c66a2fb0599f13
Robert Barabas <rbarabas@users.noreply.github.com>
2022-08-31 13:19:51 -0700

    fix: remove tracing from auth measurement

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -11,4 +11,3 @@ wget "https://go.dev/dl/go1.18.3.linux-${ARCH}.tar.gz"
 tar -C /usr/local -xzf "go1.18.3.linux-${ARCH}.tar.gz"
 rm "go1.18.3.linux-${ARCH}.tar.gz"
 export PATH=$PATH:/usr/local/go/bin
-
