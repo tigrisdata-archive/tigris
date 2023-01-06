8fefdc2aff0ed67fe00e27ecc682bf3be0beeb7e
Himank Chaudhary <himank@tigrisdata.com>
2023-01-06 09:53:51 -0800

    fix: Remove language keywords restrictions

--- test/v1/server/collection_test.go
+++ test/v1/server/collection_test.go
@@ -75,7 +75,7 @@ func TestCreateCollection(t *testing.T) {
 }
 
 func TestCreateCollectionInvalidName(t *testing.T) {
-	invalidCollectionName := []string{"", "$testcoll", "testcoll$", "test$coll", "abstract", "yield"}
+	invalidCollectionName := []string{"", "$testcoll", "testcoll$", "test$coll"}
 	for _, name := range invalidCollectionName {
 		resp := createCollection(t, "valid_db_name", name, testCreateSchema)
 		resp.Status(http.StatusBadRequest).
