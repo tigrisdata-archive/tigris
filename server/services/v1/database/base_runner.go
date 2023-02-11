520f4a43b1ab560efee4160638aa2b4421295ccb
Himank Chaudhary <himank@tigrisdata.com>
2023-02-10 16:12:48 -0800

    chore: Break down query runner into multiple files

---
+++ server/services/v1/database/base_runner.go
@@ -0,0 +1,266 @@
+// Copyright 2022-2023 Tigris Data, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package database
+
+import (
+	"context"
+
+	jsoniter "github.com/json-iterator/go"
+	"github.com/tigrisdata/tigris/errors"
+	"github.com/tigrisdata/tigris/internal"
+	"github.com/tigrisdata/tigris/keys"
+	"github.com/tigrisdata/tigris/query/filter"
+	"github.com/tigrisdata/tigris/query/sort"
+	"github.com/tigrisdata/tigris/schema"
+	"github.com/tigrisdata/tigris/server/cdc"
+	"github.com/tigrisdata/tigris/server/metadata"
+	"github.com/tigrisdata/tigris/server/metrics"
+	"github.com/tigrisdata/tigris/server/transaction"
+	"github.com/tigrisdata/tigris/server/types"
+	"github.com/tigrisdata/tigris/store/search"
+	"github.com/tigrisdata/tigris/util"
+	ulog "github.com/tigrisdata/tigris/util/log"
+	"github.com/tigrisdata/tigris/value"
+)
+
+type BaseQueryRunner struct {
+	encoder     metadata.Encoder
+	cdcMgr      *cdc.Manager
+	searchStore search.Store
+	txMgr       *transaction.Manager
+	accessToken *types.AccessToken
+}
+
+func NewBaseQueryRunner(encoder metadata.Encoder, cdcMgr *cdc.Manager, txMgr *transaction.Manager, searchStore search.Store, accessToken *types.AccessToken) *BaseQueryRunner {
+	return &BaseQueryRunner{
+		encoder:     encoder,
+		cdcMgr:      cdcMgr,
+		searchStore: searchStore,
+		txMgr:       txMgr,
+		accessToken: accessToken,
+	}
+}
+
+// getDatabase is a helper method to return database either from the transactional context for explicit transactions or
+// from the tenant object. Returns a user facing error if the database is not present.
+func (runner *BaseQueryRunner) getDatabase(_ context.Context, tx transaction.Tx, tenant *metadata.Tenant, projName string, branch string) (*metadata.Database, error) {
+	dbBranch := metadata.NewDatabaseNameWithBranch(projName, branch)
+
+	if tx != nil && tx.Context().GetStagedDatabase() != nil {
+		// this means that some DDL operation has modified the database object, then we need to perform all the operations
+		// on this staged database.
+
+		db, ok := tx.Context().GetStagedDatabase().(*metadata.Database)
+		if !ok {
+			return nil, errors.Internal("invalid transaction staged database")
+		}
+
+		if db.Name() != dbBranch.Name() {
+			return nil, errors.InvalidArgument("collections should belong to the same database branch in the transaction")
+		}
+
+		return db, nil
+	}
+
+	project, err := tenant.GetProject(projName)
+	if err != nil {
+		return nil, createApiError(err)
+	}
+
+	// otherwise, simply read from the in-memory cache/disk.
+	db, err := project.GetDatabase(dbBranch)
+	if err != nil {
+		return nil, createApiError(err)
+	}
+
+	return db, nil
+}
+
+// getCollection is a wrapper around getCollection method on the database object to return a user facing error if the
+// collection is not present.
+func (runner *BaseQueryRunner) getCollection(db *metadata.Database, collName string) (*schema.DefaultCollection, error) {
+	collection := db.GetCollection(collName)
+	if collection == nil {
+		return nil, errors.NotFound("collection doesn't exist '%s'", collName)
+	}
+
+	return collection, nil
+}
+
+func (runner *BaseQueryRunner) getDBAndCollection(ctx context.Context, tx transaction.Tx,
+	tenant *metadata.Tenant, dbName string, collName string, branch string,
+) (*metadata.Database, *schema.DefaultCollection, error) {
+	db, err := runner.getDatabase(ctx, tx, tenant, dbName, branch)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	collection, err := runner.getCollection(db, collName)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return db, collection, nil
+}
+
+func (runner *BaseQueryRunner) insertOrReplace(ctx context.Context, tx transaction.Tx, tenant *metadata.Tenant,
+	coll *schema.DefaultCollection, documents [][]byte, insert bool,
+) (*internal.Timestamp, [][]byte, error) {
+	var err error
+	ts := internal.NewTimestamp()
+	allKeys := make([][]byte, 0, len(documents))
+	for _, doc := range documents {
+		// reset it back to doc
+		doc, err = runner.mutateAndValidatePayload(coll, newInsertPayloadMutator(coll, ts.ToRFC3339()), doc)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		keyGen := newKeyGenerator(doc, tenant.TableKeyGenerator, coll.Indexes.PrimaryKey)
+		key, err := keyGen.generate(ctx, runner.txMgr, runner.encoder, coll.EncodedName)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		// we need to use keyGen updated document as it may be mutated by adding auto-generated keys.
+		tableData := internal.NewTableDataWithTS(ts, nil, keyGen.document)
+		tableData.SetVersion(coll.GetVersion())
+		if insert || keyGen.forceInsert {
+			// we use Insert API, in case user is using autogenerated primary key and has primary key field
+			// as Int64 or timestamp to ensure uniqueness if multiple workers end up generating same timestamp.
+			err = tx.Insert(ctx, key, tableData)
+		} else {
+			err = tx.Replace(ctx, key, tableData, false)
+		}
+		if err != nil {
+			return nil, nil, err
+		}
+		allKeys = append(allKeys, keyGen.getKeysForResp())
+	}
+	return ts, allKeys, err
+}
+
+func (runner *BaseQueryRunner) mutateAndValidatePayload(coll *schema.DefaultCollection, mutator mutator, doc []byte) ([]byte, error) {
+	deserializedDoc, err := util.JSONToMap(doc)
+	if ulog.E(err) {
+		return doc, err
+	}
+
+	// this will mutate map, so we need to serialize this map again
+	if err = mutator.stringToInt64(deserializedDoc); err != nil {
+		return doc, err
+	}
+
+	if err = mutator.setDefaultsInIncomingPayload(deserializedDoc); err != nil {
+		return doc, err
+	}
+
+	if err = coll.Validate(deserializedDoc); err != nil {
+		// schema validation failed
+		return doc, err
+	}
+
+	if mutator.isMutated() {
+		return util.MapToJSON(deserializedDoc)
+	}
+
+	return doc, nil
+}
+
+func (runner *BaseQueryRunner) buildKeysUsingFilter(coll *schema.DefaultCollection,
+	reqFilter []byte, collation *value.Collation,
+) ([]keys.Key, error) {
+	filterFactory := filter.NewFactory(coll.QueryableFields, collation)
+	filters, err := filterFactory.Factorize(reqFilter)
+	if err != nil {
+		return nil, err
+	}
+
+	primaryKeyIndex := coll.Indexes.PrimaryKey
+	kb := filter.NewKeyBuilder(filter.NewStrictEqKeyComposer(func(indexParts ...interface{}) (keys.Key, error) {
+		return runner.encoder.EncodeKey(coll.EncodedName, primaryKeyIndex, indexParts)
+	}))
+
+	return kb.Build(filters, coll.Indexes.PrimaryKey.Fields)
+}
+
+func (runner *BaseQueryRunner) mustBeDocumentsCollection(collection *schema.DefaultCollection, method string) error {
+	if collection.Type() != schema.DocumentsType {
+		return errors.InvalidArgument("%s is only supported on collection type of 'documents'", method)
+	}
+
+	return nil
+}
+
+func (runner *BaseQueryRunner) getSortOrdering(coll *schema.DefaultCollection, sortReq jsoniter.RawMessage) (*sort.Ordering, error) {
+	ordering, err := sort.UnmarshalSort(sortReq)
+	if err != nil || ordering == nil {
+		return nil, err
+	}
+
+	for i, sf := range *ordering {
+		cf, err := coll.GetQueryableField(sf.Name)
+		if err != nil {
+			return nil, err
+		}
+		if cf.InMemoryName() != cf.Name() {
+			(*ordering)[i].Name = cf.InMemoryName()
+		}
+
+		if !cf.Sortable {
+			return nil, errors.InvalidArgument("Cannot sort on `%s` field", sf.Name)
+		}
+	}
+	return ordering, nil
+}
+
+func (runner *BaseQueryRunner) getWriteIterator(ctx context.Context, tx transaction.Tx,
+	collection *schema.DefaultCollection, reqFilter []byte, collation *value.Collation,
+	metrics *metrics.WriteQueryMetrics,
+) (Iterator, error) {
+	var (
+		err      error
+		iKeys    []keys.Key
+		iterator Iterator
+	)
+
+	reader := NewDatabaseReader(ctx, tx)
+
+	if iKeys, err = runner.buildKeysUsingFilter(collection, reqFilter, collation); err == nil {
+		iterator, err = reader.KeyIterator(iKeys)
+	} else {
+		if iterator, err = reader.ScanTable(collection.EncodedName); err != nil {
+			return nil, err
+		}
+		filterFactory := filter.NewFactory(collection.QueryableFields, collation)
+		var filters []filter.Filter
+		if filters, err = filterFactory.Factorize(reqFilter); err != nil {
+			return nil, err
+		}
+
+		iterator, err = reader.FilteredRead(iterator, filter.NewWrappedFilter(filters))
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	if len(iKeys) == 0 {
+		metrics.SetWriteType("pkey")
+	} else {
+		metrics.SetWriteType("non-pkey")
+	}
+
+	return iterator, nil
+}
---
+++ server/services/v1/database/search_runner.go
@@ -0,0 +1,270 @@
+// Copyright 2022-2023 Tigris Data, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package database
+
+import (
+	"context"
+	"math"
+
+	api "github.com/tigrisdata/tigris/api/server/v1"
+	"github.com/tigrisdata/tigris/errors"
+	"github.com/tigrisdata/tigris/query/filter"
+	"github.com/tigrisdata/tigris/query/read"
+	qsearch "github.com/tigrisdata/tigris/query/search"
+	"github.com/tigrisdata/tigris/schema"
+	"github.com/tigrisdata/tigris/server/metadata"
+	"github.com/tigrisdata/tigris/server/metrics"
+	ulog "github.com/tigrisdata/tigris/util/log"
+	"github.com/tigrisdata/tigris/value"
+)
+
+// SearchQueryRunner is a runner used for Queries that are reads and needs to return result in streaming fashion.
+type SearchQueryRunner struct {
+	*BaseQueryRunner
+
+	req          *api.SearchRequest
+	streaming    SearchStreaming
+	queryMetrics *metrics.SearchQueryMetrics
+}
+
+// ReadOnly on search query runner is implemented as search queries do not need to be inside a transaction; in fact,
+// there is no need to start any transaction for search queries as they are simply forwarded to the indexing store.
+func (runner *SearchQueryRunner) ReadOnly(ctx context.Context, tenant *metadata.Tenant) (Response, context.Context, error) {
+	db, err := runner.getDatabase(ctx, nil, tenant, runner.req.GetProject(), runner.req.GetBranch())
+	if err != nil {
+		return Response{}, ctx, err
+	}
+
+	ctx = runner.cdcMgr.WrapContext(ctx, db.Name())
+
+	collection, err := runner.getCollection(db, runner.req.GetCollection())
+	if err != nil {
+		return Response{}, ctx, err
+	}
+
+	wrappedF, err := filter.NewFactory(collection.QueryableFields, value.NewCollationFrom(runner.req.Collation)).WrappedFilter(runner.req.Filter)
+	if err != nil {
+		return Response{}, ctx, err
+	}
+
+	searchFields, err := runner.getSearchFields(collection)
+	if err != nil {
+		return Response{}, ctx, err
+	}
+
+	facets, err := runner.getFacetFields(collection)
+	if err != nil {
+		return Response{}, ctx, err
+	}
+
+	if len(facets.Fields) == 0 {
+		runner.queryMetrics.SetSearchType("search_all")
+	} else {
+		runner.queryMetrics.SetSearchType("faceted")
+	}
+
+	fieldSelection, err := runner.getFieldSelection(collection)
+	if err != nil {
+		return Response{}, ctx, err
+	}
+
+	sortOrder, err := runner.getSortOrdering(collection, runner.req.Sort)
+	if err != nil {
+		return Response{}, ctx, err
+	}
+
+	if sortOrder != nil {
+		runner.queryMetrics.SetSort(true)
+	} else {
+		runner.queryMetrics.SetSort(false)
+	}
+
+	ctx = metrics.UpdateSpanTags(ctx, runner.queryMetrics)
+
+	pageSize := int(runner.req.PageSize)
+	if pageSize == 0 {
+		pageSize = defaultPerPage
+	}
+	var totalPages *int32
+
+	searchQ := qsearch.NewBuilder().
+		Query(runner.req.Q).
+		SearchFields(searchFields).
+		Facets(facets).
+		PageSize(pageSize).
+		Filter(wrappedF).
+		ReadFields(fieldSelection).
+		SortOrder(sortOrder).
+		Build()
+
+	searchReader := NewSearchReader(ctx, runner.searchStore, collection, searchQ)
+	var iterator *FilterableSearchIterator
+	if runner.req.Page != 0 {
+		iterator = searchReader.SinglePageIterator(collection, wrappedF, runner.req.Page)
+	} else {
+		iterator = searchReader.Iterator(collection, wrappedF)
+	}
+	if err != nil {
+		return Response{}, ctx, err
+	}
+
+	pageNo := int32(defaultPageNo)
+	if runner.req.Page > 0 {
+		pageNo = runner.req.Page
+	}
+	for {
+		resp := &api.SearchResponse{}
+		var row Row
+		for iterator.Next(&row) {
+			if searchQ.ReadFields != nil {
+				// apply field selection
+				newValue, err := searchQ.ReadFields.Apply(row.Data.RawData)
+				if ulog.E(err) {
+					return Response{}, ctx, err
+				}
+				row.Data.RawData = newValue
+			}
+
+			resp.Hits = append(resp.Hits, &api.SearchHit{
+				Data: row.Data.RawData,
+				Metadata: &api.SearchHitMeta{
+					CreatedAt: row.Data.CreateToProtoTS(),
+					UpdatedAt: row.Data.UpdatedToProtoTS(),
+				},
+			})
+
+			if len(resp.Hits) == pageSize {
+				break
+			}
+		}
+
+		resp.Facets = iterator.getFacets()
+		if totalPages == nil {
+			tp := int32(math.Ceil(float64(iterator.getTotalFound()) / float64(pageSize)))
+			totalPages = &tp
+		}
+
+		resp.Meta = &api.SearchMetadata{
+			Found:      iterator.getTotalFound(),
+			TotalPages: *totalPages,
+			Page: &api.Page{
+				Current: pageNo,
+				Size:    int32(searchQ.PageSize),
+			},
+		}
+		// if no hits, got error, send only error
+		// if no hits, no error, at least one response and break
+		// if some hits, got an error, send current hits and then error (will be zero hits next time)
+		// if some hits, no error, continue to send response
+		if len(resp.Hits) == 0 {
+			if iterator.Interrupted() != nil {
+				return Response{}, ctx, iterator.Interrupted()
+			}
+			if pageNo > defaultPageNo && pageNo > runner.req.Page {
+				break
+			}
+		}
+
+		if err := runner.streaming.Send(resp); err != nil {
+			return Response{}, ctx, err
+		}
+
+		pageNo++
+	}
+
+	return Response{}, ctx, nil
+}
+
+func (runner *SearchQueryRunner) getSearchFields(coll *schema.DefaultCollection) ([]string, error) {
+	searchFields := runner.req.SearchFields
+	if len(searchFields) == 0 {
+		// this is to include all searchable fields if not present in the query
+		for _, cf := range coll.GetQueryableFields() {
+			if cf.DataType == schema.StringType {
+				searchFields = append(searchFields, cf.InMemoryName())
+			}
+		}
+	} else {
+		for i, sf := range searchFields {
+			cf, err := coll.GetQueryableField(sf)
+			if err != nil {
+				return nil, err
+			}
+			if !cf.Indexed {
+				return nil, errors.InvalidArgument("`%s` is not a searchable field. Only indexed fields can be queried", sf)
+			}
+			if cf.InMemoryName() != cf.Name() {
+				searchFields[i] = cf.InMemoryName()
+			}
+		}
+	}
+	return searchFields, nil
+}
+
+func (runner *SearchQueryRunner) getFacetFields(coll *schema.DefaultCollection) (qsearch.Facets, error) {
+	facets, err := qsearch.UnmarshalFacet(runner.req.Facet)
+	if err != nil {
+		return qsearch.Facets{}, err
+	}
+
+	for i, ff := range facets.Fields {
+		cf, err := coll.GetQueryableField(ff.Name)
+		if err != nil {
+			return qsearch.Facets{}, err
+		}
+		if !cf.Faceted {
+			return qsearch.Facets{}, errors.InvalidArgument(
+				"Cannot generate facets for `%s`. Faceting is only supported for numeric and text fields", ff.Name)
+		}
+		if cf.InMemoryName() != cf.Name() {
+			facets.Fields[i].Name = cf.InMemoryName()
+		}
+	}
+
+	return facets, nil
+}
+
+func (runner *SearchQueryRunner) getFieldSelection(coll *schema.DefaultCollection) (*read.FieldFactory, error) {
+	var selectionFields []string
+
+	// Only one of include/exclude. Honor inclusion over exclusion
+	//nolint:gocritic
+	if len(runner.req.IncludeFields) > 0 {
+		selectionFields = runner.req.IncludeFields
+	} else if len(runner.req.ExcludeFields) > 0 {
+		selectionFields = runner.req.ExcludeFields
+	} else {
+		return nil, nil
+	}
+
+	factory := &read.FieldFactory{
+		Include: map[string]read.Field{},
+		Exclude: map[string]read.Field{},
+	}
+
+	for _, sf := range selectionFields {
+		cf, err := coll.GetQueryableField(sf)
+		if err != nil {
+			return nil, err
+		}
+
+		factory.AddField(&read.SimpleField{
+			Name: cf.Name(),
+			Incl: len(runner.req.IncludeFields) > 0,
+		})
+	}
+
+	return factory, nil
+}
