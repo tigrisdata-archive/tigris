a929f97855ff1ea2e9d858ab3b71f514788ea4df
Adil Ansari <adil@tigrisdata.com>
2023-03-26 23:03:10 -0700

    feat: Create metronome account for new namespaces

---
+++ server/services/v1/billing/metronome.go
@@ -0,0 +1,131 @@
+// Copyright 2022-2023 Tigris Data, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package billing
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	jsoniter "github.com/json-iterator/go"
+	"github.com/tigrisdata/tigris/server/config"
+	"golang.org/x/net/context/ctxhttp"
+	"github.com/tigrisdata/tigris/errors"
+)
+
+const (
+	ContentTypeJSON = "application/json"
+	TimeFormat      = time.RFC3339
+)
+
+type Metronome struct {
+	Config config.Metronome
+}
+
+func (m *Metronome) CreateAccount(ctx context.Context, namespaceId string, name string) (string, error) {
+	payload := map[string]interface{}{
+		"name":           name,
+		"ingest_aliases": []string{namespaceId},
+	}
+	req, err := m.createRequest(ctx, "/customers", payload)
+	if err != nil {
+		return "", err
+	}
+
+	respBytes, err := m.executeRequest(ctx, req)
+	if err != nil {
+		return "", err
+	}
+
+	var respBody CreateAccountResponse
+	err = jsoniter.Unmarshal(respBytes, &respBody)
+	if err != nil {
+		return "", err
+	}
+
+	return respBody.Data.Id, nil
+}
+
+type CreateAccountResponse struct {
+	Data struct {
+		Id string
+	}
+}
+
+func (m *Metronome) AddDefaultPlan(ctx context.Context, metronomeId string) (bool, error) {
+	return m.AddPlan(ctx, metronomeId, m.Config.DefaultPlan)
+}
+
+func (m *Metronome) AddPlan(ctx context.Context, metronomeId string, planId string) (bool, error) {
+	payload := map[string]any{
+		"plan_id": planId,
+		// plans can only start at UTC midnight, so we either +1 or -1 from current day
+		"starting_on": nextMidnight().Format(TimeFormat),
+	}
+
+	req, err := m.createRequest(ctx, fmt.Sprintf("/customers/%s/plans/add", metronomeId), payload)
+	if err != nil {
+		return false, err
+	}
+
+	_, err = m.executeRequest(ctx, req)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func (m *Metronome) createRequest(ctx context.Context, path string, payload map[string]any) (*http.Request, error) {
+	jsonPayload, err := jsoniter.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, m.Config.URL+path, bytes.NewBuffer(jsonPayload))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", ContentTypeJSON)
+	req.Header.Set("Authorization", "Bearer "+m.Config.ApiKey)
+	return req, nil
+}
+
+func (m *Metronome) executeRequest(ctx context.Context, req *http.Request) ([]byte, error) {
+	resp, err := ctxhttp.Do(ctx, nil, req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	respBytes, err := io.ReadAll(resp.Body)
+	if resp.StatusCode != http.StatusOK {
+		errStr := string(respBytes)
+		return nil, errors.Internal("metronome failure: '%s'", resp.Status+" - "+errStr)
+	}
+
+	if err != nil {
+		return []byte{}, err
+	}
+	return respBytes, nil
+}
+
+func nextMidnight() time.Time {
+	now := time.Now().UTC()
+	yyyy, mm, dd := now.Date()
+	return time.Date(yyyy, mm, dd+1, 0, 0, 0, 0, time.UTC)
+}
---
+++ server/services/v1/billing/metronome_test.go
@@ -0,0 +1,121 @@
+// Copyright 2022-2023 Tigris Data, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package billing
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/h2non/gock"
+	"github.com/stretchr/testify/require"
+	"github.com/tigrisdata/tigris/server/config"
+)
+
+func TestMetronome_CreateAccount(t *testing.T) {
+	defer gock.Off()
+	cfg := config.DefaultConfig.Billing.Metronome
+	metronome := &Metronome{Config: cfg}
+	ctx := context.TODO()
+
+	t.Run("creating account succeeds in metronome", func(t *testing.T) {
+		namespaceId := "nsId_123"
+		tenantName := "foo tenant"
+		gock.New(cfg.URL).
+			Post("customers").
+			MatchHeader("Authorization", cfg.ApiKey).
+			MatchHeader("Content-Type", "application/json").
+			MatchType("json").
+			JSON(map[string]interface{}{
+				"name":           tenantName,
+				"ingest_aliases": []string{namespaceId},
+			}).
+			Reply(200).
+			JSON(map[string]interface{}{
+				"data": map[string]string{
+					"id": "metronome123",
+				},
+			})
+
+		createdId, err := metronome.CreateAccount(ctx, namespaceId, tenantName)
+		require.NoError(t, err)
+		require.Equal(t, "metronome123", createdId)
+	})
+
+	t.Run("Invalid API key", func(t *testing.T) {
+		gock.New(cfg.URL).
+			Reply(401).
+			JSON(map[string]string{
+				"message": "Unauthorized",
+			})
+
+		createdId, err := metronome.CreateAccount(ctx, "nsId1", "foo_tenant")
+		require.ErrorContains(t, err, "401 Unauthorized")
+		require.Empty(t, createdId)
+	})
+
+	require.True(t, gock.IsDone())
+}
+
+func TestMetronome_AddDefaultPlan(t *testing.T) {
+	defer gock.Off()
+	cfg := config.DefaultConfig.Billing.Metronome
+	metronome := &Metronome{Config: cfg}
+	ctx := context.TODO()
+
+	t.Run("create new time", func(t *testing.T) {
+		metronomeId := "nsId_123"
+		yyyy, mm, dd := time.Now().UTC().Date()
+		expectedDate := time.Date(yyyy, mm, dd+1, 0, 0, 0, 0, time.UTC).Format(TimeFormat)
+
+		gock.New(cfg.URL).
+			Post(fmt.Sprintf("customers/%s/plans/add", metronomeId)).
+			MatchHeader("Authorization", cfg.ApiKey).
+			MatchHeader("Content-Type", "application/json").
+			MatchType("json").
+			JSON(map[string]interface{}{
+				"plan_id":     cfg.DefaultPlan,
+				"starting_on": expectedDate,
+			}).
+			Reply(200).
+			JSON(map[string]interface{}{
+				"data": map[string]string{
+					"id": "plan123",
+				},
+			})
+
+		added, err := metronome.AddDefaultPlan(ctx, metronomeId)
+		require.NoError(t, err)
+		require.True(t, added)
+	})
+
+	t.Run("bad request when no plan exists", func(t *testing.T) {
+		metronomeId := "nsId_123"
+
+		gock.New(cfg.URL).
+			Post(fmt.Sprintf("customers/%s/plans/add", metronomeId)).
+			Reply(400).
+			JSON(map[string]string{
+				"message": "No such plan",
+			})
+
+		added, err := metronome.AddDefaultPlan(ctx, metronomeId)
+		require.ErrorContains(t, err, "No such plan")
+		require.False(t, added)
+	})
+
+	require.True(t, gock.IsDone())
+}
