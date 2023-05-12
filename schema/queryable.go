aedc4031d4134633a56273a65d809a7104cb32bf
Garren <garren.smith@gmail.com>
2023-05-12 07:26:59 +0200

    fix: default false for sort and search for metadata

--- schema/queryable.go
+++ schema/queryable.go
@@ -209,21 +209,21 @@ func (builder *QueryableFieldsBuilder) BuildQueryableFields(fields []*Field, fie
 		}
 	}
 
-	ptrTrue := true
+	ptrFalse := false
 	// Allowing metadata fields to be queryable. User provided reserved fields are rejected by FieldBuilder.
 	queryableFields = append(queryableFields, builder.NewQueryableField(ReservedFields[CreatedAt], &Field{
 		FieldName:     ReservedFields[CreatedAt],
 		DataType:      DateTimeType,
-		Sorted:        &ptrTrue,
-		SearchIndexed: &ptrTrue,
+		Sorted:        &ptrFalse,
+		SearchIndexed: &ptrFalse,
 		Indexed:       &indexMetadata,
 	}, fieldsInSearch))
 
 	queryableFields = append(queryableFields, builder.NewQueryableField(ReservedFields[UpdatedAt], &Field{
 		FieldName:     ReservedFields[UpdatedAt],
 		DataType:      DateTimeType,
-		Sorted:        &ptrTrue,
-		SearchIndexed: &ptrTrue,
+		Sorted:        &ptrFalse,
+		SearchIndexed: &ptrFalse,
 		Indexed:       &indexMetadata,
 	}, fieldsInSearch))
 
