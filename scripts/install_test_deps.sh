8661eb9f706d9e4c64da7027003ee074da6c0a23
Yevgeniy Firsov <firsov@tigrisdata.com>
2023-05-24 09:33:08 -0700

    fix: Upgrade dependencies

--- scripts/install_test_deps.sh
+++ scripts/install_test_deps.sh
@@ -18,7 +18,7 @@ set -ex
 export GO111MODULE=on
 
 go install github.com/golang/mock/mockgen@v1
-go install github.com/vektra/mockery/v2@v2.20.0 # for generating mocks
+go install github.com/vektra/mockery/v2@v2 # for generating mocks
 
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)/bin" latest
 
